7d6c6f6f6104726eb341991f3070975f966b6e06
Fobianka <fobianko@gmail.com>
2023-12-10 18:03:45 +0200

    added blog page

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,12 @@
 $(function () {
 
+  $('.blog-page__slider').slick({
+    prevArrow: '<button type="button" class="slick-prev"><svg xmlns="http://www.w3.org/2000/svg" width="5" height="10"><path d="M.015 4.97a.745.745 0 01.178-.49L3.934.2a.548.548 0 01.863 0 .762.762 0 010 .98L1.486 4.97l3.31 3.79a.776.776 0 010 .99.56.56 0 01-.862 0L.193 5.47a.772.772 0 01-.178-.5zm0 0" fill-rule="evenodd"/></svg></button>',
+    nextArrow: '<button type="button" class="slick-next"><svg xmlns="http://www.w3.org/2000/svg" width="5" height="10"><path d="M5 4.99a.816.816 0 00-.17-.5L1.06.2a.556.556 0 00-.87 0 .771.771 0 000 .99l3.33 3.8-3.33 3.8a.771.771 0 000 .99.568.568 0 00.87 0l3.77-4.3A.715.715 0 005 4.99zm0 0" fill-rule="evenodd"/></svg></button>',
+    infinite: false,
+  });
+
+
   $('.oneproduct-tabs__top-item').on('click', function(e){
     e.preventDefault();
     $('.oneproduct-tabs__top-item').removeClass('oneproduct-tabs__top-item--active');
@@ -7,7 +14,6 @@ $(function () {
 
     $('.oneproduct-tabs__content-item').removeClass('oneproduct-tabs__content-item--active');
     $($(this).attr('href')).addClass('oneproduct-tabs__content-item--active');
-
   });
 
 
@@ -55,7 +61,6 @@ $(function () {
   });
   
 
-
   $('.top-slider__inner').slick({
     dots: true,
     arrows: false,
@@ -72,7 +77,8 @@ $(function () {
     starWidth: "17px",
     normalFill: "#ccccce",
     ratedFill: "#ffc35b",
-    readOnly: true
+    readOnly: true,
+    starSvg: '<svg xmlns="http://www.w3.org/2000/svg" height="16" width="18" viewBox="0 0 576 512"><path d="M316.9 18c-5.3-11-16.5-18-28.8-18s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329l-24.6 145.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329l104.2-103.1c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7l-143.7-21.2L316.9 18z"/></svg>',
   });
 
   function getTimeRemaining(endtime) {
